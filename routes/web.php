c47dddd20dac380f5ed886e3bed03570dead0b34
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2025-01-13 12:07:16 -0300

    feat: Implementando tela de encurtar link e configurando js e css para manipulação.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', [AuthController::class, 'index']);
-Route::get('/', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [AuthController::class, 'index'])->name('index');
